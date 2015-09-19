19e9137d402197a5723edc3b01fc127efaea5af0
waterwu <boatwind@gmail.com>
2015-09-19 17:43:51 +0800

    rebuild templer lib

---
+++ test/templer.coffee
@@ -0,0 +1,58 @@
+{DBStore, assert, _, User, UserDoc} = require("./base")
+
+class Templer
+    constructor:(tmpls)->
+        if _.isString(tmpls) then tmpls = {index:tmpls}
+        @tmpls = tmpls
+        return @makeMethod()
+    makeInvoke:(tmpls)->
+        invoke = (method, args...)->
+            if _.isString(method)
+                method = _.template(method)
+            data = _.extend({},tmpls,args...)
+            method(data)
+    makeMethod:()->
+        tmpls = _.extend({ invoke:@makeInvoke(tmpls) },@tmpls)
+        mainTmpl = _.template(tmpls.index) if _.isString(tmpls.index)
+        tmplMethod = (args...)=>
+            data = _.extend({args:args or []},tmpls,args...)
+            mainTmpl(data)
+        _.extend tmplMethod, tmpls,
+            extend:(newTmpls)=>
+                @extend(tmplMethod, newTmpls)
+    extend:(tmplMethod, newTmpls)->
+        newTmpls = _.extend({}, @tmpls, newTmpls)
+        newTmpls._parent = @tmpls
+        return new Templer(newTmpls)
+templer = (tmpls)->
+    return new Templer(arguments...)
+templer.extend = (tmpls)->
+    return templer(tmpls)
+# templer.extend = ()->
+
+describe "Templer", ->
+    it "should work", ->
+        tmpl = templer(index:"hello <%=world%>", world:"world")
+        assert.equal(tmpl(), "hello world")
+        newTmpl = tmpl.extend(world:tmpl.world+"2")
+        assert.equal(newTmpl(), "hello world2")
+    it "should context deliver to sub templer", ->
+        tmpl = templer
+            outsider: " [Outsider] "
+            useInIndex: templer " <%=outsider%> "
+            index:" <%=useInIndex({outsider:outsider})%> "
+        assert(tmpl().indexOf("[Outsider]"), "should pass ctx to sub tmpl")
+    it "should extend with super method", ->
+        tmpl = templer({index: "hello world"}).extend
+            index: "before <%=_parent.index%> after"
+        assert.equal(tmpl(), "before hello world after", "should wrap with super")
+    it "should invoke", ->
+        tmpl = templer({index: "hello <%=invoke(name, {msg:'invokename'})%>", name:"<%=msg%>"})
+        assert.equal(tmpl(), "hello invokename")
+    it "should use args", ->
+        tmpl = templer("<%=args.join('')%>")
+        assert.equal(tmpl(1,2,3),"123","check use args")
+    it "should use _super "
+
+
+# Templer("hello")
---
+++ test/templer.js
@@ -0,0 +1,124 @@
+var DBStore, Templer, User, UserDoc, _, assert, ref, templer,
+  slice = [].slice;
+
+ref = require("./base"), DBStore = ref.DBStore, assert = ref.assert, _ = ref._, User = ref.User, UserDoc = ref.UserDoc;
+
+Templer = (function() {
+  function Templer(tmpls) {
+    if (_.isString(tmpls)) {
+      tmpls = {
+        index: tmpls
+      };
+    }
+    this.tmpls = tmpls;
+    return this.makeMethod();
+  }
+
+  Templer.prototype.makeInvoke = function(tmpls) {
+    var invoke;
+    return invoke = function() {
+      var args, data, method;
+      method = arguments[0], args = 2 <= arguments.length ? slice.call(arguments, 1) : [];
+      if (_.isString(method)) {
+        method = _.template(method);
+      }
+      data = _.extend.apply(_, [{}, tmpls].concat(slice.call(args)));
+      return method(data);
+    };
+  };
+
+  Templer.prototype.makeMethod = function() {
+    var mainTmpl, tmplMethod, tmpls;
+    tmpls = _.extend({
+      invoke: this.makeInvoke(tmpls)
+    }, this.tmpls);
+    if (_.isString(tmpls.index)) {
+      mainTmpl = _.template(tmpls.index);
+    }
+    tmplMethod = (function(_this) {
+      return function() {
+        var args, data;
+        args = 1 <= arguments.length ? slice.call(arguments, 0) : [];
+        data = _.extend.apply(_, [{
+          args: args || []
+        }, tmpls].concat(slice.call(args)));
+        return mainTmpl(data);
+      };
+    })(this);
+    return _.extend(tmplMethod, tmpls, {
+      extend: (function(_this) {
+        return function(newTmpls) {
+          return _this.extend(tmplMethod, newTmpls);
+        };
+      })(this)
+    });
+  };
+
+  Templer.prototype.extend = function(tmplMethod, newTmpls) {
+    newTmpls = _.extend({}, this.tmpls, newTmpls);
+    newTmpls._parent = this.tmpls;
+    return new Templer(newTmpls);
+  };
+
+  return Templer;
+
+})();
+
+templer = function(tmpls) {
+  return (function(func, args, ctor) {
+    ctor.prototype = func.prototype;
+    var child = new ctor, result = func.apply(child, args);
+    return Object(result) === result ? result : child;
+  })(Templer, arguments, function(){});
+};
+
+templer.extend = function(tmpls) {
+  return templer(tmpls);
+};
+
+describe("Templer", function() {
+  it("should work", function() {
+    var newTmpl, tmpl;
+    tmpl = templer({
+      index: "hello <%=world%>",
+      world: "world"
+    });
+    assert.equal(tmpl(), "hello world");
+    newTmpl = tmpl.extend({
+      world: tmpl.world + "2"
+    });
+    return assert.equal(newTmpl(), "hello world2");
+  });
+  it("should context deliver to sub templer", function() {
+    var tmpl;
+    tmpl = templer({
+      outsider: " [Outsider] ",
+      useInIndex: templer(" <%=outsider%> "),
+      index: " <%=useInIndex({outsider:outsider})%> "
+    });
+    return assert(tmpl().indexOf("[Outsider]"), "should pass ctx to sub tmpl");
+  });
+  it("should extend with super method", function() {
+    var tmpl;
+    tmpl = templer({
+      index: "hello world"
+    }).extend({
+      index: "before <%=_parent.index%> after"
+    });
+    return assert.equal(tmpl(), "before hello world after", "should wrap with super");
+  });
+  it("should invoke", function() {
+    var tmpl;
+    tmpl = templer({
+      index: "hello <%=invoke(name, {msg:'invokename'})%>",
+      name: "<%=msg%>"
+    });
+    return assert.equal(tmpl(), "hello invokename");
+  });
+  it("should use args", function() {
+    var tmpl;
+    tmpl = templer("<%=args.join('')%>");
+    return assert.equal(tmpl(1, 2, 3), "123", "check use args");
+  });
+  return it("should use _super ");
+});
